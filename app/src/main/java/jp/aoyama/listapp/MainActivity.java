b3fc8aeb3b28c5197bcad7fb7397ab29be9e380a
kondouyuuki <yukikondo14@gmail.com>
2020-03-11 16:57:12 +0900

    second

--- app/src/main/java/jp/aoyama/listapp/MainActivity.java
+++ app/src/main/java/jp/aoyama/listapp/MainActivity.java
@@ -5,16 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.GridLayout;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView myListView;
+    GridView myGridView;
     String[] items;
     String[] prices;
     String[] descriptions;
+    ImageView flagImages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +25,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Resources res = getResources();
-        myListView = (ListView) findViewById(R.id.myListView);
-        items = res.getStringArray(R.array.items);
+        myGridView = (GridView) findViewById(R.id.myGridView);
+        items = res.getStringArray(R.array.flagName);
         prices = res.getStringArray(R.array.prices);
         descriptions = res.getStringArray(R.array.descriptions);
 
         ItemAdapter itemAdapter = new ItemAdapter(this, items, prices, descriptions);
-        myListView.setAdapter(itemAdapter);
+        myGridView.setAdapter(itemAdapter);
+
+
+
+
 
     }
 }
